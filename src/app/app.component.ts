19156fe82db3187125c5b6a1115b0b6e2a3eefd9
Aziza Kalmasova <aziza.kalmasova@phystech.edu>
2019-10-09 16:18:47 +0300

    toolbar changed, new class hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ConcepTIS';
+  hero: Hero = {
+    id: 14,
+    name: 'Aziza',
+  };
+
+  constructor() { }
+
+  ngOnInit(){
+
+  }
 }
